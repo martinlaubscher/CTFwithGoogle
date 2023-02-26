ede73233f4879932a25b13127711fdcb868f911f
Martin Laubscher <martin.laubscher@me.com>
2023-02-26 02:53:42 +0000

    typo in comments renamed directories for consistency

--- challenge-8/pngflag.sh
+++ challenge-8/pngflag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#run from directory where hideandseek.jpg is located
+#run from directory where hideandseek.png is located
 xxd -p hideandseek.png | tr -d '\n' | grep -Po '65444948\K(..)' | tr '\n' ' ' | xxd -r -p | base64 -d
 echo -e
