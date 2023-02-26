e92d6d8de3e109fc5c012b824b3ed59904b8032c
Martin Laubscher <martin.laubscher@me.com>
2023-02-26 23:57:40 +0000

    removed unnecessary truncation

--- challenge-8/pngflag2.sh
+++ challenge-8/pngflag2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #run from directory where hideandseek.png is located
-strings hideandseek.png | tr -d '\n' | grep -Po 'DIH\K(.)' | tr -d '\n' | base64 -d
+strings hideandseek.png | grep -Po 'DIH\K(.)' | tr -d '\n' | base64 -d
 echo -e
