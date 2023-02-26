2e600ab5c2e22f7aab70a79757fa8033f940b779
Martin Laubscher <113263884+martinlaubscher@users.noreply.github.com>
2023-02-26 18:24:29 +0000

    'strings' instead of 'xxd'
    another solution for challenge 8

--- challenge-8/pngflag2.sh
+++ challenge-8/pngflag2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#run from directory where hideandseek.jpg is located
+#run from directory where hideandseek.png is located
 strings hideandseek.png | tr -d '\n' | grep -Po 'DIH\K(.)' | tr -d '\n' | base64 -d
 echo -e
